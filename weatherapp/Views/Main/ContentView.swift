2adc3f3c9f37ddf878d28e6034734fe3f2966b6e
Esther Larose <63347102+estherlarose@users.noreply.github.com>
2024-08-10 08:20:11 -0600

    Add files via upload

---
+++ weatherapp/Views/Main/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  weatherapp
+//
+//  Created by Esther Larose on 8/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weatherapp/weatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappApp.swift
+//  weatherapp
+//
+//  Created by Esther Larose on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
